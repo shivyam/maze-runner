4ff49e416a7d02202cd5026c6e5f2e9cd5c3ae04
shivyam <shivyamehta1@gmail.com>
2024-01-21 18:38:46 -0500

    Maze data stored in ArrayList (closes #4)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,11 +1,14 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.*;
+
+
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
@@ -17,6 +20,8 @@ public class Main {
         options.addOption("i", true, "input maze filepath");
         options.addOption("p", true, "user path guess");
 
+        ArrayList<ArrayList<String>> userMaze = new ArrayList<ArrayList<String>>();
+
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine cmd = parser.parse(options, args);
@@ -27,21 +32,33 @@ public class Main {
             logger.info("**** Reading the maze from file " + inputFilePath);
             BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
             String line;
+            
+
             while ((line = reader.readLine()) != null) {
+                ArrayList<String> row = new ArrayList<String>();
                 for (int idx = 0; idx < line.length(); idx++) {
+                    
                     if (line.charAt(idx) == '#') {
                         System.out.print("WALL ");
+                        row.add("WALL ");
                     } else if (line.charAt(idx) == ' ') {
                         System.out.print("PASS ");
+                        row.add("PASS ");
                     }
+                
                 }
                 System.out.print(System.lineSeparator());
+                userMaze.add(row);
             }
         } catch(Exception e) {
             logger.info("/!\\ An error has occured /!\\");
         }
+
+        Maze mazeTest= new Maze(userMaze);
+
         logger.info("**** Computing path");
-        logger.info("PATH NOT COMPUTED");
+        logger.info(mazeTest.findCanonicalPath());
         logger.info("** End of MazeRunner");
+        
     }
 }
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -0,0 +1,60 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.util.ArrayList;
+
+public class Maze{
+
+    private ArrayList<ArrayList<String>> maze= new ArrayList<ArrayList<String>>();
+
+    public Maze(ArrayList<ArrayList<String>> userMaze){
+        maze= new ArrayList<ArrayList<String>>(userMaze);
+
+    }
+
+    public int findEntryTile(){
+        return 1;
+    }
+
+    //finds path in the form "FFFRRFFLLF"
+    public String findCanonicalPath(){
+        String path="";
+        return "FFFF";
+    }
+
+    //outputs path in the form "3F2R2F2LF"
+    public String findFactorizedPath(){
+        return "4F";
+    }
+
+
+    //outputs string message verifying if user input path is correct
+    public String checkPath(String userPath){
+        String rightPath= findCanonicalPath();
+        boolean flag= false;
+
+        //implement to check if user path is valid
+        if(userPath==rightPath){
+            flag= true;
+        }
+
+        if(flag){
+            return "correct path";
+        }
+
+        return "incorrect path";
+        
+    }
+
+    //to verify maze ArrayList correctly contained maze
+    // public void printMaze(){
+    //     for (int i=0; i<maze.size(); i++) {
+    //         ArrayList<String> row = new ArrayList<String>(maze.get(i));
+    //         for (int j=0; j<row.size(); j++) {
+    //             System.out.print(row.get(j)); 
+    //         }
+    //         System.out.println("");
+    //     }
+        
+    // }
+
+}
